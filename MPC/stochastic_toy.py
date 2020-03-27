cf2bef65dacad3649bd97015fe6f9d549fe8c040
LongntLe <long@minerva.kgi.edu>
2020-03-26 21:02:33 -0700

    Update and rename particle-filtering.py to stochastic_toy.py

--- MPC/stochastic_toy.py
+++ MPC/stochastic_toy.py
@@ -4,10 +4,10 @@ import cvxpy as cvx
 from sklearn.linear_model import LinearRegression
 import time
 
-w = 60 # parameter for calibrating period. I.e., the bot look into the data 15 data points before the current point to project market moving in the next window.
+w = 15 # parameter for calibrating period. I.e., the bot look into the data 15 data points before the current point to project market moving in the next window.
 
-s = ob_df.ms.tolist()[:200]# load data into list
-act_s = ob_df.ms.tolist()[1:201] # data moves up 1 step to account for latency.
+s = ob_df.ms.tolist()[:1000]# load data into list
+act_s = ob_df.ms.tolist()[1:1001] # data moves up 1 step to account for latency.
 ds = np.array(act_s[1:]) - np.array(act_s[:-1])
 
 # fit trend line on moving window
@@ -25,19 +25,23 @@ for i in range(w, len(s)):
     print (np.mean(ds_t), np.std(ds_t))
 
   # create a set of projected data
+    projs = []
     for _ in range(50):
+        # create projected time series
         nxt_inc = np.random.normal(loc=np.mean(ds_t), scale=np.std(ds_t), size=w+1)
         proj_s = nxt_inc + np.array([model.coef_[0]*i + model.intercept_ for i in range(w, 2*w+1)])
+        # process time series for analysis later
+        proj_s_org = proj_s
+        proj_s_org = proj_s_org.T
         proj_s = proj_s[1:] - proj_s[:-1]
         proj_s = proj_s.T
 
         # solve optimization problem on a window of time T=15
-        #print (sum(ord_list))
         """
         The code block below makes use of CVXPy to solve an optimization.
         """
         # inputs:
-        N = 60 # size of moving window
+        N = 15 # size of moving window
         max_pos = 1 # max position size
 
         # matrices A and B as shown in MPC model formulation
@@ -53,36 +57,34 @@ for i in range(w, len(s)):
         con.extend([cvx.norm(X[0,:],'inf')<=1]) # constraint 4: maximum inventory size
 
         con.extend([X[0, -1] == 0]) #neutral exposure
-        
-        
-        #con.extend([np.std(proj_s*X[0,1:16]) < 0.5])
-        con.extend([cvx.min(cvx.multiply(proj_s, X[0,1:w+1])) >= -0.5])
-        obj = cvx.Maximize(sum(proj_s*X[0,1:w+1]) + cvx.min(cvx.multiply(proj_s, X[0,1:w+1])))
-        #obj = cvx.Maximize(sum(proj_s*X[0,1:16])-np.std(np.cumsum(proj_s*X[0,1:16]))) # objective function
+      
+        obj = cvx.Maximize(X[0,1:w+1]@proj_s-cvx.tv(cvx.diag(proj_s_org[1:]*X[0,1:w+1])))
 
         prob = cvx.Problem(obj, con) # insert objective function and constraints into cvxpy
 
         #output
         prob.solve() # solve using cvxpy magic
         tmp_ord_list.append(U.value[0][0])
-    v = np.mean(tmp_ord_list)
-    print (v)
-    if abs(v) <= 0.0:
-        v = 0
+        
+    v = np.mean(tmp_ord_list) # record the mean of all optimal controls
+    #print (v)
+
     ord_list.append(v) # the bot executes only the first action, the window moves the process repeats
     
 ord_list = ord_list[1:] # discard the first (zero place-holder) action
 
-# # plot result (inventory over time)
-# plt.plot(list(range(14, 30)), X.value[0])
-
 # plot the pnl of the bot
 ord_list = [0]*w + ord_list
 pnl = np.cumsum(np.cumsum(np.array(ord_list[:-1]))*ds)
+
+fig = plt.figure(figsize=(15,5))
+plt.subplot(1,3,1)
 plt.plot(pnl)
 
 # plot inventory over time
+plt.subplot(1,3,2)
 plt.plot(np.cumsum(ord_list))
 
 # plot price time series
+plt.subplot(1,3,3)
 plt.plot(act_s)
